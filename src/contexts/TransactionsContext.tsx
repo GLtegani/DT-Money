c3b310e1c5a03f77a6290f8644d014532fc27d20
Gabriel Tegani <gabrielbtegani@gmail.com>
2024-06-06 18:23:36 -0300

    feat: Refactor TransactionsContext to use useCallback for fetchTransactions and createTransaction
    This commit refactors the TransactionsContext component in the TransactionsContext.tsx file. The fetchTransactions and createTransaction functions are now wrapped with useCallback to optimize performance and prevent unnecessary re-renders.

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useState, useCallback } from "react";
 import { api } from "../lib/axios";
 import { createContext } from "use-context-selector";
 
@@ -33,35 +33,39 @@ export const TransactionsContext = createContext({} as TransactionContextType)
 export function TransactionsProvider({children}: TransactionsProviderProps) {
    const [transactions, setTransactions] = useState<Transaction[]>([])
 
-   const fetchTransactions = async(query?: string) => {
-      const response = await api.get('transactions', {
-         params: {
-            _sort: 'createdAt',
-            _order: 'desc',
-            q: query,
-         }
-      })
-      
-      setTransactions(response.data)
-   }
-
-   const createTransaction = async(data: CreateTransactionInput) => {
-      const { description, category, price, type } = data
-
-      const response = await api.post('transactions', {
-         description,
-         price,
-         category,
-         type,
-         createdAt: new Date(),
-      })
-
-      setTransactions(state => [response.data, ...state])
-   }
+   const fetchTransactions = useCallback(
+      async(query?: string) => {
+         const response = await api.get('transactions', {
+            params: {
+               _sort: 'createdAt',
+               _order: 'desc',
+               q: query,
+            }
+         })
+         
+         setTransactions(response.data)
+      }, []
+   )
+   const createTransaction = useCallback(
+      async(data: CreateTransactionInput) => {
+         const { description, category, price, type } = data
+   
+         const response = await api.post('transactions', {
+            description,
+            price,
+            category,
+            type,
+            createdAt: new Date(),
+         })
+   
+         setTransactions(state => [response.data, ...state])
+      },
+      []
+   )
 
    useEffect(() => {
       fetchTransactions()
-   }, [])
+   }, [fetchTransactions])
 
    return (
       <TransactionsContext.Provider value={{
